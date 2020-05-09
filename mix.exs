822fb546a91afe0ec6e447a72d70cd5f1f41dca4
Steven Nunez <steven.nunez@gmail.com>
2020-05-09 10:47:06 -0400

    Add Markdown Lib

--- mix.exs
+++ mix.exs
@@ -46,7 +46,9 @@ defmodule Seance.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      { :earmark, "> 1.4.0" }
+
     ]
   end
 
