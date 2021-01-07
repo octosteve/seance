844361313f939d7d4592d473b3a281bc5f060b55
Steven Nunez <octosteve@github.com>
2021-01-07 17:11:44 -0500

    Update Docker file and render post chunk

--- lib/seance_web/live/home_live/home_live.ex
+++ lib/seance_web/live/home_live/home_live.ex
@@ -4,7 +4,17 @@ defmodule SeanceWeb.HomeLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    posts = Blog.list_posts()
+    posts =
+      Blog.list_posts()
+      |> Enum.map(fn post ->
+        body =
+          for %{"content" => content, "type" => "markdown"} <- post.body, reduce: "" do
+            acc -> acc <> to_string(content)
+          end
+
+        %{post | body: body}
+      end)
+
     {:ok, assign(socket, posts: posts)}
   end
 end
